ea8dd45e07dc85cdc7fc3b151e2e9b6fcbec518f
Jacob <dev.zomerfeld@gmail.com>
2021-03-14 18:58:53 +0000

    add:New props, clickOutside directive, clear btn

--- src/components/SimpleSelect.vue
+++ src/components/SimpleSelect.vue
@@ -1,64 +1,170 @@
 <template>
-  <div class="vss">
-    <div class="vss__options">
+  <div
+    ref="vssSelect"
+    v-click-out="closeDropdown"
+    class="vss"
+    :class="{ 'vss--disabled': disabled }"
+  >
+    <div
+      ref="vssSelectOptions"
+      class="vss__options"
+      :class="{ vss__active: isOpen, 'vss__options--shadow': shadow }"
+      :aria-label="placeholder"
+      role="select"
+    >
       <div
         v-for="option in options"
-        @mousedown="selectOption"
         :key="option.text"
+        ref="vssSelectOption"
         class="vss__option"
+        role="option"
+        type="button"
+        @mousedown="selectOption"
       >
         <input
+          :id="`option-${option.value}`"
           class="vss__option-input"
           type="radio"
           :value="option.value"
-          :id="`option-${option.value}`"
+          :disabled="disabled"
           name="selectGroup"
         />
-
         {{ option.text }}
       </div>
     </div>
-    <div class="vss__selected" @mousedown="toggleDropdown($event)">
-      {{ selected.text }}
+    <div
+      class="vss__selected"
+      type="button"
+      @mousedown="toggleDropdown($event)"
+    >
+      <span
+        v-show="selected.value && floatingPlaceholder"
+        class="vss__floating-label"
+        >{{ placeholder }}</span
+      >
+      <span class="vss__selected-label">
+        {{ selected.text ? selected.text : placeholder }}
+      </span>
     </div>
+    <span
+      v-show="selected.value && clearBtn"
+      class="vss__clear-button"
+      @click.prevent="clearSelection"
+      >X</span
+    >
   </div>
 </template>
 
 <script>
 export default {
   name: "SimpleSelect",
+  directives: {
+    "click-out": {
+      bind: function (el, binding, vnode) {
+        el.eventSetDrag = function () {
+          el.setAttribute("data-dragging", "yes");
+        };
+        el.eventClearDrag = function () {
+          el.removeAttribute("data-dragging");
+        };
+        el.eventOnClick = function (event) {
+          var dragging = el.getAttribute("data-dragging");
+          // Check that the click was outside the el and its children,  wasn't a drag
+          if (!(el == event.target || el.contains(event.target)) && !dragging) {
+            // call method provided in attribute value
+            vnode.context[binding.expression](event);
+          }
+        };
+        document.addEventListener("touchstart", el.eventClearDrag);
+        document.addEventListener("touchmove", el.eventSetDrag);
+        document.addEventListener("click", el.eventOnClick);
+        document.addEventListener("touchend", el.eventOnClick);
+      },
+      unbind: function (el) {
+        document.removeEventListener("touchstart", el.eventClearDrag);
+        document.removeEventListener("touchmove", el.eventSetDrag);
+        document.removeEventListener("click", el.eventOnClick);
+        document.removeEventListener("touchend", el.eventOnClick);
+        el.removeAttribute("data-dragging");
+      },
+    },
+  },
   props: {
     placeholder: {
       type: String,
       default: "Select option",
     },
+    emptyPlaceholder: {
+      type: String,
+      default: "No options",
+    },
     options: {
       type: Array,
-      default: () => [
-        { text: "Option 1", value: 1 },
-        { text: "Option 2", value: 2 },
-        { text: "Option 3", value: 3 },
-        { text: "Option 4", value: 4 },
-        { text: "Option 5", value: 5 },
-      ],
-      // required: true
+      default: () => [],
+      required: true,
+    },
+    /*
+     *  @Style - Show a shadow under the dropdown
+     */
+    shadow: {
+      type: Boolean,
+      default: true,
+    },
+    /*
+     *  @Style - Show floating placeholder text when selection is done
+     */
+    floatingPlaceholder: {
+      type: Boolean,
+      default: false,
+    },
+    /*
+     *  @Function - disable component
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    /*
+     *  @Function + @Style - Show clear button as 'x' to clear selected option
+     */
+    clearBtn: {
+      type: Boolean,
+      default: false,
     },
   },
   data() {
     return {
-      selected: { text: null, value: 0 },
+      selected: { text: null, value: null },
+      noOptions: false,
+      isOpen: false,
+      isClosed: true,
     };
   },
   created() {
-    if (!this.selected.text) this.selected.text = this.placeholder;
+    this.checkOptions();
   },
   methods: {
+    checkOptions() {
+      this.noOptions = this.options?.length === 0 || false;
+    },
+    setPlaceholder() {
+      if (this.noOptions) {
+        this.selected.text = this.emptyPlaceholder;
+        return;
+      }
+      this.selected.text = this.placeholder;
+    },
     toggleDropdown() {
-      const selectOptions = document.querySelector(".vss__options");
-      selectOptions.classList.toggle("vss__active");
+      if (this.noOptions) return;
+      this.isOpen = !this.isOpen;
+
+      // for future use, handle the check if dropdown is closed
+      this.isClosed = !this.isClosed;
+    },
+    closeDropdown() {
+      if (this.isOpen) this.isOpen = false;
     },
     selectOption(option) {
-      console.log(option);
       const optionValue = option.target.childNodes[0].value;
       const optionText = option.target.childNodes[1].data.trim();
 
@@ -68,12 +174,23 @@ export default {
       this.toggleDropdown();
     },
     updateValue(value) {
-      this.$emit("select-value", value);
+      if (value) {
+        this.$emit("input", value);
+        return;
+      }
+    },
+    clearSelection() {
+      this.closeDropdown();
+      this.resetSelectedModel();
+      this.$emit("clear", null);
+    },
+    resetSelectedModel() {
+      this.selected = { text: null, value: null };
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
- @import '@/scss/vss.scss';
+@import "@/scss/vss.scss";
 </style>
