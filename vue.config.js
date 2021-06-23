eb55f2b0999a8fe4006c8f3047d66986558751fe
devzom <dev.zomerfeld@gmail.com>
2021-06-23 21:19:20 +0200

    Add outputDir -docs for GitHub pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-simpleselect/'
-    : '/',
-  devServer: {
-    disableHostCheck: true,
-  },
+	publicPath: process.env.NODE_ENV === "production" ? "/vue-simpleselect/" : "",
+	outputDir: "docs",
+	devServer: {
+		disableHostCheck: true,
+	},
 };
